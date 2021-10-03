074c16d131752b04d471eabebc41d0520dd4e5b6
Iva Lesperance Charles <Iva.LesperanceCharles@mail.citytech.cuny.edu>
2021-10-02 20:45:10 -0400

    city search implemented - Professor: Molina and TA: Silu

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -2,26 +2,35 @@ import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Form from 'react-bootstrap/Form'
+import Container  from 'react-bootstrap/Container';
+import Row  from 'react-bootstrap/Row';
+import Col  from 'react-bootstrap/Col';
+
 function City(props) {
   return (
-    <Card  border="primary" className="m-1" >
-    <Card.Body>
-      <Card.Title>{props.data.City}</Card.Title>
-    </Card.Body>
-    <ListGroup className="list-group-flush" >
-      <ListGroup.Item action variant="info" >Population: {props.data.EstimatedPopulation}</ListGroup.Item>
-      <ListGroup.Item action variant="info" >Location: {props.data.LocationType}</ListGroup.Item>
-      <ListGroup.Item action variant="info" >ZipCode Type: {props.data.ZipCodeType}</ListGroup.Item>
-    </ListGroup>
-  </Card>
-    );
+    <Card border="primary" className="m-1" >
+      <Card.Body>
+        <Card.Title>{props.data.City}</Card.Title>
+      </Card.Body>
+      <ListGroup className="list-group-flush" >
+        <ListGroup.Item action variant="info" >Population: {props.data.EstimatedPopulation}</ListGroup.Item>
+        <ListGroup.Item action variant="info" >Location: {props.data.LocationType}</ListGroup.Item>
+        <ListGroup.Item action variant="info" >ZipCode Type: {props.data.ZipCodeType}</ListGroup.Item>
+      </ListGroup>
+    </Card>
+  );
+}
+function ZipCode(props){
+  return(
+      <Col className="border p-2 m-2 bg-light text-center">{props.zipCode}</Col>
+  );
 }
 
 function ZipSearchField(props) {
   return (
     <Form >
       <Form.Group className="col-md-6 mx-auto mb-3" controlId="searchForm.ControlInput1">
-        <Form.Control size="lg" className="mb-2" type="search" placeholder="Enter a five digit zip code" onChange={(e) => props.handleChange(e)} />
+        <Form.Control size="lg" className="mb-2" type="search" placeholder="Enter a five digit zip code or city name" onChange={(e) => props.handleChange(e)} />
       </Form.Group>
     </Form>
   );
@@ -31,7 +40,8 @@ function ZipSearchField(props) {
 class App extends Component {
   state = {
     zipCode: '',
-    cities: [],
+    citiesDatas: [],
+    zipCodes: [],
   }
 
   zipChange = (event) => {
@@ -40,38 +50,72 @@ class App extends Component {
 
     this.setState({ zipCode: event.target.value })
 
-    if(event.target.value.length === 5) {
+    if(event.target.value.length === 5 && !isNaN(this.filterInt(event.target.value))) {
       fetch('https://ctp-zip-api.herokuapp.com/zip/'+event.target.value)
         .then(res => res.json())
         .then(datas => {
           console.log(datas)
           // save the data to my state,
           // and in the render function diplay the <City /> components dynamically
-          // this.setState({ cities: data }) // 1
-          this.setState( { cities: datas } );
+          // this.setState({ citiesDatas: data }) // 1
+          this.setState( { citiesDatas: datas } );
         })
         .catch(err => {
-          this.setState({ cities: [] })
+          this.setState({ citiesDatas: [] })
+        })
+    }else if(this.filterStr(event.target.value) && event.target.value.length > 2 ){
+      fetch('https://ctp-zip-api.herokuapp.com/city/'+event.target.value.toUpperCase())
+        .then(res => res.json())
+        .then(datas => {
+          console.log(datas)
+          // save the data to my state,
+          // and in the render function diplay the <City /> components dynamically
+          // this.setState({ zipCodes: data }) // 1
+          this.setState( { zipCodes: datas } );
+        })
+        .catch(err => {
+          this.setState({ zipCodes: [] })
         })
     } else {
-      this.setState({ cities: [] })
+      this.setState({ citiesDatas: [] , zipCodes: []})
     }
   }
+
   
   render() {
     return (
       <Container className="border p-5" >
         <Row className="p-3">
-          <h2 className="mx-auto"> Zip Code Search</h2>
+          <h2 className="mx-auto"> Zip Code / City Search</h2>
         </Row>
         <ZipSearchField handleChange={ (e) => this.zipChange(e) } />
         <p className="col-md-6 mx-auto">Current Zip code is: { this.state.zipCode }</p>
         <Row xs={1} md={2} lg={5} className="justify-content-between" >
-            { this.state.cities.map(city => <City data={city} />) }
+            { this.state.citiesDatas.map(city => <City data={city} />) }
+        </Row>
+        <Row xs={4} md={5} lg={6} className="justify-content-between" >
+          {this.state.zipCodes.map(code => <ZipCode zipCode={code} />)}
         </Row>
       </Container>
     );
   }
+// this function filter the input for a number amd return a number value  or not a number, NaN.
+  filterInt(value) {
+    if (/^[-+]?(\d+|Infinity)$/.test(value)) {
+      return Number(value)
+    } else {
+      return NaN
+    }
+  }
+// this function filter the input for a string amd return true or false.
+  filterStr(value) {
+    if (/^[a-zA-Z]*$/.test(value)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
 
 export default App;
